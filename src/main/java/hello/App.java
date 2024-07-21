070ab2a54cf2964c3f2308de55a2be6413eba3ca
manoprajith <manoprajith1500@gmail.com>
2024-07-21 14:54:40 +0530

    added slack message

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.cicd.my-project;
 
 /**
  * Hello world!
--- src/test/java/hello/AppTest.java
+++ src/test/java/hello/AppTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.cicd.my-project;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
